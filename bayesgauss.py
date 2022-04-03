f6752440606f7380f6cfdca18102184bb1f42b7f
FEDERICO INNOCENTI <55858422+fedeghigo@users.noreply.github.com>
2022-04-03 22:00:40 +0200

    implemented the description of the methods

--- bayesgauss.py
+++ bayesgauss.py
@@ -8,18 +8,40 @@ from sklearn.metrics import accuracy_score
 
 class BayesGaussian(object):
     def __init__(self,df,  x_train, y_train, x_test, y_test) -> None:
+        """
+        Initialization of the Naive Bayes Gaussian instance.
+        Parameters
+        ----------
+        dataset : pd.DataFrame
+        
+        """
         self.df=df
         self.x_train = x_train
         self.y_train = y_train
         self.x_test = x_test
         self.y_test = y_test
+        
+        if not isinstance(df, pd.DataFrame):
+                raise ValueError("The dataset must be a pandas DataFrame.")
+     
 
-    def _calc_class_prior(self, test_value=1):
+    def _calc_class_prior(self, test_value=1)-> float:
+        """
+        Calculation of prior probability for test 1 or 0.
+        Parameters
+        ----------
+        test_value : int
+        
+        """
         outcome_count = sum(self.x_train.iloc[:,-1] == test_value)
         self.class_priors = outcome_count / len(self.y_train)
         return self.class_priors
     
     def subset(self):
+        """
+        Creation of Subset
+            
+        """
         self.x_train_1=self.x_train[self.x_train["diagnosis"]==1]
         self.x_train_0=self.x_train[self.x_train["diagnosis"]==0]
         self.y_train_1=self.y_train[self.y_train["diagnosis"]==1]
@@ -27,7 +49,13 @@ class BayesGaussian(object):
         
         return self.x_train_1, self.x_train_0 , self.y_train_1 ,self.y_train_0
 
-    def mean(self, df) :
+    def mean(self, df) -> float:
+        """
+        Method for calculate the Mean.
+        Parameters
+        ----------
+        
+        """
         self.calculated_mean = []
         for i in self.df.columns:
             x_sum = sum(df[i])
@@ -35,15 +63,30 @@ class BayesGaussian(object):
             self.calculated_mean.append(temp)
         return self.calculated_mean
 
-    def stdev(self,df ):
+    def stdev(self,df )-> float:
+        """
+        Method for calculate the Standard deviation.
+        Parameters
+        ----------
+        
+        """
         self.calculated_std = []
         for i in self.x_train.columns:
             x_std = np.std(df[i])
             self.calculated_std.append(x_std)
         return self.calculated_std
 
-    # Calculate the mean, stdev and count for each column in a dataset
+    
     def summarize_dataset(self):
+        """
+        Method for calculate the first Summary.
+        
+        Output 
+        ----------
+        -Mean 
+        -Dev std 
+        -Len of Training
+        """
         return (
             print("Mean of the training  x", self.calculated_mean),
             print("Std of the training x", self.calculated_std),
@@ -51,12 +94,26 @@ class BayesGaussian(object):
         )
 
     def calculate_probability(self, x, mean, stdev):
+        """
+        Method for calculate Gaussian.
+        
+        Parameters 
+        ----------
+        -Mean 
+        -Dev std 
+        -x
+        """
         exponent = np.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
         return (1 / (np.sqrt(2 * np.pi) * stdev)) * exponent
     
     
   
     def likelihood_list(self, df):
+        """
+        Method for create a list of Likelihood.
+        
+        
+        """
         self.likelihood_computed = pd.DataFrame()
         concat=pd.DataFrame() 
         mean_temp=self.mean(df)[:-1]
@@ -77,10 +134,20 @@ class BayesGaussian(object):
         return self.likelihood_computed #self.likelihood_computed
     
     def single_likelihood(self, df ):
+        """
+        Method for calculate single Likelihood from the columns.
+        
+        
+        """
         mul_likelihood = self.likelihood_list(df).prod(axis=1) 
         return mul_likelihood
         
     def bayes_classification(self):
+        """
+        Method Bayes Algorithm.
+        
+        
+        """
         self.y_computed = []
         likeli = np.array(self.single_likelihood(self.x_train_1))
         likeli_0 =np.array(self.single_likelihood(self.x_train_0))
@@ -92,6 +159,14 @@ class BayesGaussian(object):
         return self.y_computed
     
     def normalize_data(self, threshold=0.5):
+        """
+        Method Normalization of the Bayes Algorithm.
+        
+        Parameters
+        -----------------------
+        Threshold default 0.5
+        
+        """
         self.pred=pd.Series(self.y_computed)
         self.pred[self.pred>=threshold]=1
         self.pred[self.pred<threshold]=0
@@ -99,6 +174,14 @@ class BayesGaussian(object):
         
         
     def summary_bayes_classification(self,threshold=0.5):
+        """
+        Method for the summary of the Bayes Algorithm.
+        
+        Parameters
+        -----------------------
+        Threshold default 0.5
+        
+        """
         print("...... Mean .......")
         print("Train 1",self.mean(self.x_train_1) )
         print("Train 0",self.mean(self.x_train_0) )
